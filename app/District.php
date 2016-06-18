f4c354503f4878d0e2a0d227113039c10a1d1991
Hosmel Quintana 😴 <hosmelq@gmail.com>
2016-06-17 21:34:58 -0600

    use SoftDeletes trait

--- app/District.php
+++ app/District.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class District extends Model
 {
+    use SoftDeletes;
+
     /**
      * {@inheritdoc}
      */
